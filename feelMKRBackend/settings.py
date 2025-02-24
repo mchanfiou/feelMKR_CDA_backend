2578c84867580c432644c91cff3474855815b693
mchanfiou <chanfioumoustapha@gmail.com>
2025-02-24 23:13:43 +0100

    render3

--- feelMKRBackend/settings.py
+++ feelMKRBackend/settings.py
@@ -54,7 +54,7 @@ REST_FRAMEWORK = {
     ),
 }
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1'] 
+ALLOWED_HOSTS = ["feelmkr-cda-backend.onrender.com", "127.0.0.1", "localhost"]
 
 # DATABASE_URL = os.getenv('DATABASE_URL')
 
